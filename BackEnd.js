71083ee2fac652d8006d36cf6bb5772b96f2a868
Mitchell Talyat <mitchell.talyat@gmail.com>
2021-04-20 15:22:19 -0500

    Update BackEnd.js
    Removed debug alert()

--- BackEnd.js
+++ BackEnd.js
@@ -29,8 +29,6 @@ function voteButton(clicked_id) {
     var roundid = parseInt(clicked_id[3]);
     var teamid = parseInt(clicked_id.substring(5, clicked_id.length + 1));
 
-    alert(teamid);
-
     //TODO: vote for team here
     advanceTeam(id, roundid, teamid);
 }
